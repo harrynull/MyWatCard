873605ac03d564af94ba68e7af85cb78f07141e8
Harry Null <harryyunull@gmail.com>
2019-09-07 12:51:45 -0400

    Icon Map

--- app/src/main/java/tech/harrynull/mywatcard/Transaction.kt
+++ app/src/main/java/tech/harrynull/mywatcard/Transaction.kt
@@ -1,7 +1,12 @@
 package tech.harrynull.mywatcard
 
-class Transaction(val date: String, val amount: String, val account: String,
+val ICON_MAP = mapOf("LAUNDRY" to "")
+
+class Transaction(watCard: WatCard, val date: String, val amount: String, val accountId: String,
                   val units: String, val type: String, val terminal: String) {
+    val account: Account? = watCard.findAccount(accountId)
+
     fun getAmountAsDouble() = amount.replace("$","").replace(",","").toDouble()
     fun getShortTerminalName() = terminal.substringAfter(": ")
+    fun getIcon() = ICON_MAP.filter { terminal.contains(it.key) }.entries.firstOrNull()?.value ?: "Default"
 }
