4dead10b0e5a53fd471f1c6523c118c68bf6e0de
CarbonylGroup <carbonylgp@gmail.com>
2019-09-07 15:09:55 -0400

    UI

--- app/src/main/java/tech/harrynull/mywatcard/MainActivity.kt
+++ app/src/main/java/tech/harrynull/mywatcard/MainActivity.kt
@@ -2,18 +2,23 @@ package tech.harrynull.mywatcard
 
 import android.app.AlertDialog
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.login_dialog.view.*
+import androidx.recyclerview.widget.DividerItemDecoration
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
-    fun getPreferences(database: String = "") : SharedPreferences =
-        if(database != "") getSharedPreferences(database, MODE_PRIVATE)
+    fun getPreferences(database: String = ""): SharedPreferences =
+        if (database != "") getSharedPreferences(database, MODE_PRIVATE)
         else PreferenceManager.getDefaultSharedPreferences(applicationContext)
 
     fun setPreference(key: String, value: Any, database: String = "") {
@@ -28,14 +33,27 @@ class MainActivity : AppCompatActivity() {
             is Long -> edit.putLong(key, value)
             is Boolean -> edit.putBoolean(key, value)
             is Float -> edit.putFloat(key, value)
-            else -> throw IllegalArgumentException(String.format("Type of value unsupported key=%s, value=%s", key, value))
+            else -> throw IllegalArgumentException(
+                String.format(
+                    "Type of value unsupported key=%s, value=%s",
+                    key,
+                    value
+                )
+            )
         }
         edit.apply()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(main_toolbar)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         if (getPreferences("account").getString("username", "") == "") {
             // construct view
             val dialog = this.layoutInflater.inflate(R.layout.login_dialog, null)
@@ -56,15 +74,23 @@ class MainActivity : AppCompatActivity() {
             watcard.fetch()
             var logStr = ""
             logStr += "LOG: \n"
-            for (account in watcard.accounts!!){
-                if(account.amount != "$0.00")
+            for (account in watcard.accounts!!) {
+                if (account.amount != "$0.00")
                     logStr += "${account.name}: ${account.getAmountAsDouble()}\n"
             }
             runOnUiThread {
-                //log.text = logStr
-                transaction_recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                val itemDecorator = DividerItemDecoration(
+                    this,
+                    DividerItemDecoration.VERTICAL
+                )
+                itemDecorator.setDrawable(ContextCompat.getDrawable(this, R.drawable.divider)!!)
+                transaction_recycler.layoutManager = LinearLayoutManager(
+                    this,
+                    LinearLayoutManager.VERTICAL,
+                    false
+                )
                 transaction_recycler.adapter = TransactionListAdapter(watcard.transactions)
-                transaction_recycler.invalidate()
+                transaction_recycler.addItemDecoration(itemDecorator)
             }
         }.start()
     }
--- app/src/main/java/tech/harrynull/mywatcard/TransactionListAdapter.kt
+++ app/src/main/java/tech/harrynull/mywatcard/TransactionListAdapter.kt
@@ -1,16 +1,28 @@
 package tech.harrynull.mywatcard
 
-import android.support.v7.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import kotlinx.android.synthetic.main.balance_header.view.*
 import kotlinx.android.synthetic.main.transaction_item.view.*
 
 class TransactionListAdapter(private var transactions: List<Transaction>?)
     : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    companion object {
+        private val HEADER_VIEW = 1
+//        private val FOOTER_VIEW = 2
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == HEADER_VIEW)
+            return BalanceHeader(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.balance_header,
+                    parent,
+                    false))
         return TransactionHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.transaction_item,
@@ -20,24 +32,49 @@ class TransactionListAdapter(private var transactions: List<Transaction>?)
         )
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as TransactionHolder).bind(transactions!![position])
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0) return HEADER_VIEW
+//        if (position == itemCount - 1) return FOOTER_VIEW
+        return super.getItemViewType(position)
     }
 
-    override fun getItemCount(): Int = transactions?.size ?: 0
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is TransactionHolder) {
+            holder.bind(transactions!![position - 1])
+        } else if (holder is BalanceHeader) {
+            holder.bind()
+        } else {
+            //
+        }
+    }
+
+    override fun getItemCount(): Int = (transactions?.size ?: -1) + 1
 
     inner class TransactionHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        private val transactionAccountView: TextView = itemView.transaction_account
-        private val transactionAmountView: TextView = itemView.transaction_amount
-        private val transactionDateView: TextView = itemView.transaction_date
-        private val transactionTerminalView: TextView = itemView.transaction_terminal
+//        private val transactionAccountView: TextView = itemView.transaction_account
+        private val transactionAmountView: TextView = itemView.transaction_item_amount
+        private val transactionTimeView: TextView = itemView.transaction_item_time
+        private val transactionDescriptionView: TextView = itemView.transaction_item_description
 
         fun bind(transaction: Transaction) {
-            transactionAccountView.text = transaction.account
+//            transactionAccountView.text = transaction.account
             transactionAmountView.text = transaction.amount
-            transactionDateView.text = transaction.date
-            transactionTerminalView.text = transaction.terminal
+            transactionTimeView.text = transaction.date
+            transactionDescriptionView.text = transaction.terminal
+        }
+    }
+
+    inner class BalanceHeader(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val balanceDollarView: TextView = itemView.balance_dollar_view
+        private val balanceCentView: TextView = itemView.balance_cent_view
+        private val balanceCurrencyView: TextView = itemView.balance_currency_view
+        private val balanceAccountView: TextView = itemView.balance_account_view
+
+        fun bind() {
+            balanceDollarView.text = "1,093"
+            balanceCentView.text = "99"
+            balanceCurrencyView.text = "$"
+            balanceAccountView.text = "FLEX"
         }
     }
 }
